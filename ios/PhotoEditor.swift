b52b66357a07625f7ad7c4e9efd32efe78206c59
Lane Lu <161492667+llucn@users.noreply.github.com>
2025-03-19 18:27:48 +0800

    Edit photo ios (#2)
    * feat: edit photo on ios
    
    * build: remove native folder in example
    
    * minor: format code

--- ios/PhotoEditor.swift
+++ ios/PhotoEditor.swift
@@ -1,3 +1,10 @@
+//
+//  PhotoEditor.swift
+//  PhotoEditor
+//
+//  Created by Donquijote on 27/07/2021.
+//
+
 import Foundation
 import UIKit
 import Photos
@@ -6,51 +13,54 @@ import AVFoundation
 //import ZLImageEditor
 import ExpoModulesCore
 
-public class ExpoPhotoEditModule: Module {
+public enum ImageLoad: Error {
+    case failedToLoadImage(String)
+}
+
+@objc(PhotoEditor)
+class PhotoEditor: NSObject, ZLEditImageControllerDelegate {
     var window: UIWindow?
     var bridge: RCTBridge!
     
-    var promise: Promise!
-    var delegate: EditImageDelegate!
-  
-    public func definition() -> ModuleDefinition {
-        Name("ExpoPhotoEdit")
-        
-        AsyncFunction("open") { (options: PhotoEditOptions, promise: Promise) in
-        
-            // handle path
-            guard let path = options.path else {
-                promise.reject("DONT_FIND_IMAGE", "Dont find image")
-                return;
-            }
-            
-            self.delegate = EditImageDelegate()
-            self.delegate.promise = promise
-            
-            getUIImage(url: path) { image in
-                DispatchQueue.main.async {
-                    //  set config
-                    self.setConfiguration(options: options, promise: promise)
-                    self.presentController(image: image)
-                }
-            } reject: {_ in
-                promise.reject("LOAD_IMAGE_FAILED", "Load image failed: " + path)
-            }
-        }
+    var resolve: RCTPromiseResolveBlock!
+    var reject: RCTPromiseRejectBlock!
     
+    @objc(open:withResolver:withRejecter:)
+    func open(options: NSDictionary, resolve:@escaping RCTPromiseResolveBlock,reject:@escaping RCTPromiseRejectBlock) -> Void {
+        
+        // handle path
+        guard let path = options["path"] as? String else {
+            reject("DONT_FIND_IMAGE", "Dont find image", nil)
+            return;
+        }
+        
+        getUIImage(url: path) { image in
+            DispatchQueue.main.async {
+                //  set config
+                self.setConfiguration(options: options, resolve: resolve, reject: reject)
+                self.presentController(image: image)
+            }
+        } reject: {_ in
+            reject("LOAD_IMAGE_FAILED", "Load image failed: " + path, nil)
+        }
     }
     
-    private func setConfiguration(options: PhotoEditOptions, promise: Promise) -> Void{
-        self.promise = promise;
+    func onCancel() {
+        self.reject("USER_CANCELLED", "User has cancelled", nil)
+    }
+    
+    private func setConfiguration(options: NSDictionary, resolve:@escaping RCTPromiseResolveBlock,reject:@escaping RCTPromiseRejectBlock) -> Void{
+        self.resolve = resolve;
+        self.reject = reject;
         
         // Stickers
-        let stickers = options.stickers ?? []
+        let stickers = options["stickers"] as? [String] ?? []
         ZLImageEditorConfiguration.default().imageStickerContainerView = StickerView(stickers: stickers)
         
         
         //Config
         ZLImageEditorConfiguration.default().editDoneBtnBgColor = UIColor(red:255/255.0, green:238/255.0, blue:101/255.0, alpha:1.0)
-        
+
         ZLImageEditorConfiguration.default().editImageTools = [.draw, .clip, .filter, .imageSticker, .textSticker]
         
         //Filters Lut
@@ -66,22 +76,22 @@ public class ExpoPhotoEditModule: Module {
         if let controller = UIApplication.getTopViewController() {
             controller.modalTransitionStyle = .crossDissolve
             
-            ZLEditImageViewController.showEditImageVC(parentVC:controller , image: image, delegate: self.delegate) { [weak self] (resImage, editModel) in
+            ZLEditImageViewController.showEditImageVC(parentVC:controller , image: image, delegate: self) { [weak self] (resImage, editModel) in
                 let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
                 
                 let destinationPath = URL(fileURLWithPath: documentsPath).appendingPathComponent(String(Int64(Date().timeIntervalSince1970 * 1000)) + ".png")
                 
                 do {
                     try resImage.pngData()?.write(to: destinationPath)
-                    self?.promise.resolve(destinationPath.absoluteString)
+                    self?.resolve(destinationPath.absoluteString)
                 } catch {
                     debugPrint("writing file error", error)
                 }
             }
         }
     }
-  
-  
+    
+    
     private func getUIImage (url: String ,completion:@escaping (UIImage) -> (), reject:@escaping(String)->()){
         if let path = URL(string: url) {
             SDWebImageManager.shared.loadImage(with: path, options: .continueInBackground, progress: { (recieved, expected, nil) in
@@ -101,21 +111,20 @@ public class ExpoPhotoEditModule: Module {
             reject("false")
         }
     }
-
-}
-
-internal class EditImageDelegate: NSObject, ZLEditImageControllerDelegate {
-    var promise: Promise!
     
-    public func onCancel() {
-        self.promise.reject("USER_CANCELLED", "User has cancelled")
-    }
 }
 
-internal struct PhotoEditOptions: Record {
-    @Field
-    var path: String?
-
-    @Field
-    var stickers: [String]?
+extension UIApplication {
+    class func getTopViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        
+        if let nav = base as? UINavigationController {
+            return getTopViewController(base: nav.visibleViewController)
+        } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return getTopViewController(base: selected)
+        } else if let presented = base?.presentedViewController {
+            return getTopViewController(base: presented)
+        }
+        
+        return base
+    }
 }
